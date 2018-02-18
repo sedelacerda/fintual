786986e6389506bf8edca53034f979898f764c78
SEBASTIAN DE LA CERDA <sedelacerda@uc.cl>
2018-02-18 16:36:11 -0300

    Add some style

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,4 +1,8 @@
 class Deal < ApplicationRecord
   belongs_to :portfolio
   belongs_to :stock
+
+  def trx_type
+    self.is_sale? ? 'Sale' : 'Buy'
+  end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,11 +3,9 @@ class Stock < ApplicationRecord
     def price(date_time=DateTime.now)
         stock_base = 0
         self.name.each_byte{|b| stock_base += b}
-        stock_base = stock_base % 80000.0
-        date_time_base = date_time.to_time.to_i % 20.0
+        stock_base = stock_base % 80000.00
+        date_time_base = date_time.to_time.to_i % 20.00
 
-        ap stock_base
-        ap date_time_base
-        (stock_base + date_time_base)/10.00
+        ('%.2f' % ((stock_base + date_time_base)/100.00)).to_d
     end
 end
